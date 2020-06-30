f72810c8ec47000fd5aeb527e2b916cf09eb3bfd
LihuaDawson <lihua@gmail.com>
2020-06-30 22:29:27 +0000

    double delaration of temperature, fixed it.

--- conditionals.js
+++ conditionals.js
@@ -23,7 +23,7 @@ console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
-const temperature = 12;
+
 
 if(temperature < -40 || temperature > 40) {
 console.log("Maybe going outside isn't such a great idea...");
